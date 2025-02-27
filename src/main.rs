d03d1b098567cbd34a0d138201f33ea6fba334d0
Valantine Suh <148695389+Valsuh45@users.noreply.github.com>
2025-02-27 11:36:12 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::env;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut output = File::create("/github/workspace/output.txt").expect("Failed to create output file");
+
     if args.len() != 3 {
-        println!("Enter exactly two arguments.Invalid input!!!");
+        writeln!(output, "Enter exactly two arguments. Invalid input!!!").unwrap();
+        println!("Enter exactly two arguments. Invalid input!!!");
     } else {
         let argument_1 = &args[1];
         let argument_2 = &args[2];
+
+        writeln!(output, "{}", argument_1).unwrap();
+        writeln!(output, "{}", argument_2).unwrap();
+
         println!("Hello, world!");
         println!("{}", argument_1);
         println!("{}", argument_2);
